3bc7465d5175b9579b4d226fbe5b5cd686826fdc
Yann Pouillon <notifications@materialsevolution.es>
2016-02-03 16:38:58 +0100

    Completed fake library reading and writing text files

--- tests/WIEN2k/fescdf_geometry.F90
+++ tests/WIEN2k/fescdf_geometry.F90
@@ -17,23 +17,23 @@ module fescdf_geometry
     end type fescdf_geometry_t
 
     interface
-        subroutine escdf_geometry_new(geometry) bind(c)
+        subroutine escdf_geometry_new(geometry, path, mode) bind(c)
             import
             type(c_ptr) :: geometry
+            character(kind=c_char) :: path(*)
+            character(kind=c_char) :: mode(*)
         end subroutine escdf_geometry_new
     end interface
     interface
-        subroutine escdf_geometry_read_metadata(geometry, path) bind(c)
+        subroutine escdf_geometry_read_metadata(geometry) bind(c)
             import
             type(c_ptr), value :: geometry
-            character(kind=c_char) :: path(*)
         end subroutine escdf_geometry_read_metadata
     end interface
     interface
-        subroutine escdf_geometry_write_metadata(geometry, path) bind(c)
+        subroutine escdf_geometry_write_metadata(geometry) bind(c)
             import
             type(c_ptr), value :: geometry
-            character(kind=c_char) :: path(*)
         end subroutine escdf_geometry_write_metadata
     end interface
     interface
@@ -42,29 +42,37 @@ module fescdf_geometry
             type(c_ptr), value :: geometry
         end subroutine escdf_geometry_free
     end interface
+    interface
+        subroutine escdf_geometry_set_file(geometry, path, mode) bind(c)
+            import
+            type(c_ptr), value :: geometry
+            character(kind=c_char) :: path(*)
+            character(kind=c_char) :: mode(*)
+        end subroutine escdf_geometry_set_file
+    end interface
 
 contains
 
-    subroutine fescdf_geometry_new(geometry)
+    subroutine fescdf_geometry_new(geometry, path, mode)
         type(fescdf_geometry_t), intent(inout) :: geometry
+        character(kind=c_char, len=*), intent(in) :: path
+        character(kind=c_char, len=*), intent(in) :: mode
 
-        call escdf_geometry_new(geometry%ptr)
+        call escdf_geometry_new(geometry%ptr, path, mode)
 
     end subroutine fescdf_geometry_new
 
-    subroutine fescdf_geometry_read_metadata(geometry, path)
+    subroutine fescdf_geometry_read_metadata(geometry)
         type(fescdf_geometry_t), intent(inout) :: geometry
-        character(kind=c_char, len=*), intent(in) :: path
 
-        call escdf_geometry_read_metadata(geometry%ptr, path)
+        call escdf_geometry_read_metadata(geometry%ptr)
 
     end subroutine fescdf_geometry_read_metadata
 
-    subroutine fescdf_geometry_write_metadata(geometry, path)
+    subroutine fescdf_geometry_write_metadata(geometry)
         type(fescdf_geometry_t), intent(in) :: geometry
-        character(kind=c_char, len=*), intent(in) :: path
 
-        call escdf_geometry_write_metadata(geometry%ptr, path)
+        call escdf_geometry_write_metadata(geometry%ptr)
 
     end subroutine fescdf_geometry_write_metadata
 
@@ -75,5 +83,14 @@ contains
         geometry%ptr = C_NULL_PTR
 
     end subroutine fescdf_geometry_free
+
+    subroutine fescdf_geometry_set_file(geometry, path, mode)
+        type(fescdf_geometry_t), intent(inout) :: geometry
+        character(kind=c_char, len=*), intent(in) :: path
+        character(kind=c_char, len=*), intent(in) :: mode
+
+        call escdf_geometry_set_file(geometry%ptr, path, mode)
+
+    end subroutine fescdf_geometry_set_file
 
 end module fescdf_geometry
--- tests/WIEN2k/test_escdf_fake.F90
+++ tests/WIEN2k/test_escdf_fake.F90
@@ -5,14 +5,17 @@ program testf_escdf_fake
 
     implicit none
 
-    character(kind=c_char, len=*), parameter :: test_inp = C_CHAR_"test-read.tmp"//C_NULL_CHAR
-    character(kind=c_char, len=*), parameter :: test_out = C_CHAR_"test-write.tmp"//C_NULL_CHAR
+    character(kind=c_char, len=*), parameter :: test_read = C_CHAR_"test-read.tmp"//C_NULL_CHAR
+    character(kind=c_char, len=*), parameter :: test_write = C_CHAR_"test-write.tmp"//C_NULL_CHAR
+    character(kind=c_char, len=*), parameter :: test_rmode = C_CHAR_"r"//C_NULL_CHAR
+    character(kind=c_char, len=*), parameter :: test_wmode = C_CHAR_"w+"//C_NULL_CHAR
 
     type(fescdf_geometry_t) :: geometry
 
-    call fescdf_geometry_new(geometry)
-    call fescdf_geometry_read_metadata(geometry, test_inp)
-    call fescdf_geometry_write_metadata(geometry, test_out)
+    call fescdf_geometry_new(geometry, test_read, test_rmode)
+    call fescdf_geometry_read_metadata(geometry)
+    call fescdf_geometry_set_file(geometry, test_write, test_wmode)
+    call fescdf_geometry_write_metadata(geometry)
     call fescdf_geometry_free(geometry)
 
 end program testf_escdf_fake
